a6604888bb6849256fb3d9d0094b5544af32800e
MateuszBa <mateusz.babul@gmail.com>
2022-06-07 17:19:55 +0200

    feat: deleted django project to start a new one

--- codestats/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class RepoUrlForm(forms.Form):
-    repo_url = forms.CharField(max_length=250, label="Url do repozytorium")
-
-
-class RepoZipForm(forms.Form):
-    repo_url = forms.FileField(label="Zip z repozytorium")
--- codestats/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ReportView
-
-urlpatterns = [
-    path('', ReportView.as_view(), name="report"),
-]
