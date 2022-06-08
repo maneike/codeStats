9897074a27976b6d17a9a3cdc23dde07568c0a49
MateuszBa <mateusz.babul@gmail.com>
2022-06-08 17:03:22 +0200

    feat: Added table changes to admin panel

--- codestatsapi/api/admin.py
+++ codestatsapi/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Repositories, Authors, Branches, Commits
+from .models import Repositories, Authors, Branches, Commits, Changes
 
 admin.site.register(Repositories)
 admin.site.register(Authors)
 admin.site.register(Branches)
 admin.site.register(Commits)
+admin.site.register(Changes)
