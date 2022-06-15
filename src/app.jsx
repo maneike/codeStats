0a5afe3016e4f78beb75f72b92b398d7d2b6f2c5
Martin Chelminiak <mc@tonik.pl>
2022-06-15 16:01:34 +0200

    feat: replace axios with redaxios

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,28 @@
 import { useState } from "preact/hooks";
 import FileUploader from "./components/FileUploader";
-import axios from "axios";
+
+import axios from "redaxios";
 
 export function App() {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const submitForm = () => {
-        const formData = new FormData();
-        formData.append("name", name);
-        formData.append("file", selectedFile);
-        axios
-            .post("localhost:3000", formData)
-            .then((res) => {
-                alert("File Upload success");
-            })
-            .catch((err) => alert("File Upload Error"));
-    };
+  const [selectedFile, setSelectedFile] = useState(null);
+  const submitForm = () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+    axios
+      .post("localhost:3000", formData)
+      .then((res) => {
+        alert("File Upload success");
+      })
+      .catch((err) => alert("File Upload Error"));
+  };
 
-    return (
-        <>
-            <FileUploader
-                onFileSelectSuccess={(file) => setSelectedFile(file)}
-                onFileSelectError={({ error }) => alert(error)}
-            />
-        </>
-    );
+  return (
+    <>
+      <FileUploader
+        onFileSelectSuccess={(file) => setSelectedFile(file)}
+        onFileSelectError={({ error }) => alert(error)}
+      />
+    </>
+  );
 }
