ac68fe8044eca677eee4a963d361b494d0bc3a37
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2022-06-15 15:51:34 +0200

    feat/introduce-file-uploader

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,27 @@
-import { Logo } from "./logo";
+import { useState } from "preact/hooks";
+import FileUploader from "./components/FileUploader";
+import axios from "axios";
 
 export function App() {
-  return (
-    <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
-    </>
-  );
+    const [selectedFile, setSelectedFile] = useState(null);
+    const submitForm = () => {
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("file", selectedFile);
+        axios
+            .post("localhost:3000", formData)
+            .then((res) => {
+                alert("File Upload success");
+            })
+            .catch((err) => alert("File Upload Error"));
+    };
+
+    return (
+        <>
+            <FileUploader
+                onFileSelectSuccess={(file) => setSelectedFile(file)}
+                onFileSelectError={({ error }) => alert(error)}
+            />
+        </>
+    );
 }
