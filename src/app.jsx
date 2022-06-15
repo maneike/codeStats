c6712c7e0eac30da8cab0e8e7dc5b6345d569a62
Martin Chelminiak <mc@tonik.pl>
2022-06-15 16:10:29 +0200

    refactor: cleanup

--- src/app.jsx
+++ src/app.jsx
@@ -7,22 +7,24 @@ export function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const submitForm = () => {
     const formData = new FormData();
-    formData.append("name", name);
     formData.append("file", selectedFile);
     axios
       .post("localhost:3000", formData)
       .then((res) => {
-        alert("File Upload success");
+        alert("File Upload success", res);
       })
-      .catch((err) => alert("File Upload Error"));
+      .catch((err) => alert("File Upload Error", err));
   };
 
   return (
     <>
-      <FileUploader
-        onFileSelectSuccess={(file) => setSelectedFile(file)}
-        onFileSelectError={({ error }) => alert(error)}
-      />
+      <form>
+        <FileUploader
+          onFileSelectSuccess={(file) => setSelectedFile(file)}
+          onFileSelectError={({ error }) => alert(error)}
+        />
+        <button onClick={submitForm}>Submit</button>
+      </form>
     </>
   );
 }
--- src/components/FileUploader/FileUploader.jsx
+++ src/components/FileUploader/FileUploader.jsx
@@ -1,41 +1,19 @@
-import { useState, useRef } from "preact/hooks";
-
 const FileUploader = ({ onFileSelectError, onFileSelectSuccess }) => {
-    const fileInput = useRef(null);
+  const handleFileInput = (e) => {
+    const file = e.target.files[0];
 
-    const handleFileInput = (e) => {
-        const file = e.target.files[0];
+    if (file.size > 1024)
+      onFileSelectError({
+        error: "File size cannot exceed more than 1MB",
+      });
+    else onFileSelectSuccess(file);
+  };
 
-        if (file.size > 1024)
-            onFileSelectError({
-                error: "File size cannot exceed more than 1MB",
-            });
-        else onFileSelectSuccess(file);
-    };
-
-    return (
-        <div className="file-uploader">
-            <input type="file" onChange={handleFileInput} />
-            <button
-                onClick={(e) => fileInput?.current?.click()}
-                className="btn btn-primary"
-            >
-                Upload
-            </button>
-        </div>
-    );
+  return (
+    <div className="file-uploader">
+      <input type="file" onChange={handleFileInput} />
+    </div>
+  );
 };
 
 export default FileUploader;
-
-const submitForm = () => {
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("file", selectedFile);
-    axios
-        .post(UPLOAD_URL, formData)
-        .then((res) => {
-            alert("File Upload success");
-        })
-        .catch((err) => alert("File Upload Error"));
-};
