a478e7ce25cac53072186d518007ba8c0af41d6a
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2022-06-19 19:35:16 +0200

    feat: update dependencies and aliases

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 // @ts-check
-import preactRefresh from '@prefresh/vite'
+import preactRefresh from "@prefresh/vite";
 
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
-  jsx: 'preact',
-  plugins: [preactRefresh()]
-}
+  jsx: "preact",
+  plugins: [preactRefresh()],
+  alias: {
+    react: "preact/compat",
+    "react-dom": "preact/compat",
+  },
+};
 
-export default config
+export default config;
