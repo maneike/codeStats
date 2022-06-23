8804f93709ae98d6a9b922f2542872e1e2a5e3c3
Martin Chelminiak <mc@tonik.pl>
2022-06-23 19:17:10 +0200

    fix/h5-bug

--- frontend/react_app/src/components/FileUploader/FileUploader.jsx
+++ frontend/react_app/src/components/FileUploader/FileUploader.jsx
@@ -28,7 +28,7 @@ const FileUploader = ({ onFileSelectError, onFileSelectSuccess }) => {
       <h2>Drag & Drop</h2>
       <p>or</p>
       <FileInputStyled type="file" onChange={handleFileInput} />
-      <Htag>Supports: ZIP containing .git</Htag>
+      <H5>Supports: ZIP containing .git</H5>
     </>
   );
 };
@@ -43,11 +43,10 @@ const FileInputStyled = styled.input`
 `;
 
 const CloudSVG = styled.div`
-style="display:flex; justify-content:center;"
+  display: flex;
+  justify-content: center;
 `;
 
-const Htag = styled.h5`
-  margin: 0;
-  margin-bottom: 1em;
-  opacity: 0.7;
+const H5 = styled.h5`
+  color: lightgray;
 `;
