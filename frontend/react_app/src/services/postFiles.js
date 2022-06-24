8564462f5eda914871284398ca6dc6d2d9341990
Martin Chelminiak <mc@tonik.pl>
2022-06-24 02:09:14 +0200

    feat: render fetched repos data

--- frontend/react_app/src/services/postFiles.js
+++ frontend/react_app/src/services/postFiles.js
@@ -1,11 +1,12 @@
 import axios from "redaxios";
 
-export const postFiles = (selectedFile, formData) => {
+export const postFiles = (selectedFile, formData, setFetchedRepos) => {
   formData.append("file", selectedFile);
   axios
     .post("http://localhost:80/api/zip/", formData)
     .then((res) => {
       alert("File upload success ✔", res);
+      setFetchedRepos(res.data);
     })
     .catch((err) => alert("File upload error ✘", err));
 };
