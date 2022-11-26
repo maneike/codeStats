bb460b879496cba5232442b81f312e27929505ae
Mateusz Babul <mateusz.babul@apator.com>
2022-11-26 11:29:39 +0100

    feat: Django wmails settings

--- backend/codestatsapi/settings.py
+++ backend/codestatsapi/settings.py
@@ -141,3 +141,11 @@ CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
