dcf145376a07f455758354569732bf58c1200f75
Martin Chełminiak <mc@tonik.pl>
2022-12-05 19:00:05 +0100

    feat: add line styling

--- frontend/src/components/UrlsToMergeList/UrlsToMergeList.jsx
+++ frontend/src/components/UrlsToMergeList/UrlsToMergeList.jsx
@@ -5,25 +5,26 @@ import Ul from "../Ul";
 import Li from "../Li";
 
 import { regex } from "../../helpers/extractRepoNameFromUrl";
+
 const UrlsToMergeList = ({ repoUrlsToMerge }) => {
   const repoNames = repoUrlsToMerge.map((repoUrl) => {
     const repoName = regex.exec(repoUrl)[5].replace(".git", "");
     return repoName;
   });
+
   return (
     <List>
       <Ul>
-        {repoNames.map((url, index) => (
-          <Li>
-            <StyledDropdownSelect>
-              <option key={`mergeItem-url-${index}`}>{url}</option>
-            </StyledDropdownSelect>
+        {repoNames.map((url) => (
+          <StyledLi>
+            {url}
+            <Line />
             <StyledDropdownSelect>
               {repoNames.map((everyUrl, index) => (
                 <option key={`allMergeItem-url-${index}`}>{everyUrl}</option>
               ))}
             </StyledDropdownSelect>
-          </Li>
+          </StyledLi>
         ))}
       </Ul>
     </List>
@@ -32,8 +33,18 @@ const UrlsToMergeList = ({ repoUrlsToMerge }) => {
 
 const List = styled.div``;
 
-const StyledDropdownSelect = styled(DropdownSelect)`
-  width: 40%;
+const StyledLi = styled(Li)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
+const StyledDropdownSelect = styled(DropdownSelect)`
+  width: max-content;
+`;
+
+const Line = styled.hr`
+  width: 100%;
+  margin: 0 32px;
+`;
 export default UrlsToMergeList;
