fbf397637cc4b44f5648702416cf2008fa9fa31d
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2023-01-24 00:26:22 +0100

    feat: update file upload

--- frontend/src/services/postFiles.js
+++ frontend/src/services/postFiles.js
@@ -1,18 +1,25 @@
 import axios from "redaxios";
 
 export const postFiles = (
+  receivers,
   selectedFile,
   formData,
   setFetchedRepos,
   setLoading
 ) => {
   formData.append("file", selectedFile);
+  formData.append("receivers", receivers);
   axios
     .post("http://localhost:80/api/zip/", formData)
     .then((res) => {
       alert("File upload success ✔", res);
       setFetchedRepos(res.data);
+      setLoading(false);
     })
-    .catch((err) => alert("File upload error ✘", err))
+    .catch((err) => {
+      alert("File upload error ✘", err);
+      setLoading(false);
+      throw new Error(err);
+    })
     .finally(() => setLoading(false));
 };
