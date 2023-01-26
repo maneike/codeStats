929cc05ff242ea001f59c036e6df1d910ecccb3c
Nikkoro <56053287+Nikkoro@users.noreply.github.com>
2023-01-26 06:59:28 +0100

    feat: new vite config for setting up vitest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,43 @@
-// @ts-check
+import { createRequire } from "module";
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 import preactRefresh from "@prefresh/vite";
+import alias from "@rollup/plugin-alias";
 
-/**
- * @type { import('vite').UserConfig }
- */
-const config = {
-  jsx: "preact",
-  plugins: [preactRefresh()],
-  alias: {
-    react: "preact/compat",
-    "react-dom/test-utils": "preact/test-utils",
-    "react-dom": "preact/compat",
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+  plugins: [
+    preactRefresh(),
+    alias({
+      entries: [
+        { find: "react", replacement: "preact/compat" },
+        { find: "react-dom/test-utils", replacement: "preact/test-utils" },
+        { find: "react-dom", replacement: "preact/compat" },
+      ],
+    }),
+    preact({
+      babel: {
+        plugins: ["@emotion"],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.ts",
   },
-};
-
-export default config;
+  resolve: {
+    alias: [
+      {
+        find: "preact/hooks",
+        replacement: require.resolve("preact/hooks"),
+      },
+      {
+        find: "@testing-library/preact",
+        replacement: require.resolve("@testing-library/preact"),
+      },
+    ],
+  },
+});
